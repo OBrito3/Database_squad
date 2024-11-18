d0fcc020f7f86f89dc13306ec9038e9e1892ab16
oBrito3 <obritoperez@gmail.com>
2024-11-18 13:35:32 +0000

    First commit

---
+++ index.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+const { checkConnection, syncModels } = require('./database/index')
+const addRelationsToModels = require('./database/models')
+
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+
+async function checkAndSyncPostgreSQL() {
+  await checkConnection()
+  addRelationsToModels()
+  await syncModels('alter')
+}
+
+function initializeAndListenWithExpress() {
+  const app = express()
+    .use(cors())
+    .use(morgan('dev'))
+    .use(express.json())
+    .use('/api', require('./api/routes'))
+
+    .listen(process.env.SERVER_PORT, () => {
+      console.log(`> Listening on port: ${process.env.SERVER_PORT}`)
+    })
+}
+
+async function startAPI() {
+  await checkAndSyncPostgreSQL()
+  initializeAndListenWithExpress()
+}
+
+startAPI()
